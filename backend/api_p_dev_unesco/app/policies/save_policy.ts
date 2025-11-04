825f8253ef56fe93ad1d47864a5c5d123db40b19
Maël <m01@naudet.ch>
2025-11-04 10:50:47 +0100

    feat(controllers): added the controller for the saves

---
+++ backend/api_p_dev_unesco/app/policies/save_policy.ts
@@ -0,0 +1,19 @@
+import User from '#models/user'
+import Save from '#models/save'
+import { BasePolicy } from '@adonisjs/bouncer'
+import type { AuthorizerResponse } from '@adonisjs/bouncer/types'
+
+export default class SavePolicy extends BasePolicy {
+  private async isOwner(user: User, save: Save): Promise<boolean> {
+    const rightUser = await User.query()
+      .where('id', save.userFK)
+      .where('id', user.id)
+      .select('id')
+      .first()
+    return !!rightUser
+  }
+
+  async delete(user: User, save: Save) {
+    return this.isOwner(user, save)
+  }
+}
