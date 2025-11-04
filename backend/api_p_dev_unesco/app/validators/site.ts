9727ef2fae48dee17db414d0f8ea37185b7ff63e
Maël <m01@naudet.ch>
2025-11-04 10:38:34 +0100

    fix(validators): they are in the right place now.

--- backend/api_p_dev_unesco/app/validators/site.ts
+++ backend/api_p_dev_unesco/app/validators/site.ts
@@ -1,1 +1,21 @@
 import vine from '@vinejs/vine'
+
+const getSitesQueryValidator = vine.compile(
+  vine.object({
+    page: vine.number().min(1).optional(),
+    limit: vine.number().min(1).max(100).optional(),
+    sort: vine.string().in(['name', 'country_fk', 'type_fk']).optional(), //Sort by name, country or type
+    order: vine.string().in(['asc', 'desc']).optional(), // sort order
+    countryFK: vine.number().exists(async (db, value) => {
+      const country = await db.from('countries').where('id', value).first()
+      return !!country
+    }),
+    typeFK: vine.number().exists(async (db, value) => {
+      const type = await db.from('types').where('id', value).first()
+      return !!type
+    }),
+    search: vine.string().trim().minLength(1).optional(),
+  })
+)
+
+export { getSitesQueryValidator }
