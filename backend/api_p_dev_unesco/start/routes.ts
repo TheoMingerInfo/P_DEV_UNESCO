eba4b7e6a1896322efc6f86f705a56928cf5cd79
Maël <m01@naudet.ch>
2025-11-04 09:21:45 +0100

    feat(routes): added all routes to the routes file

--- backend/api_p_dev_unesco/start/routes.ts
+++ backend/api_p_dev_unesco/start/routes.ts
@@ -8,9 +8,53 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+// Authentication paths
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('user')
+
+// User
+router
+  .group(() => {
+    router.get(':user_id', [UsersController, 'index'])
+    router.put(':user_id', [UsersController, 'update']).use(middleware.auth())
+    router.delete(':user_id', [UsersController, 'destroy']).use(middleware.auth())
+  })
+  .prefix('users')
+
+// See saves per users
+router.get('users/:user_id/saves', [UserSavesController, 'show'])
+
+// Add and delete saves
+router
+  .group(() => {
+    router.post('sites/:site_id/saves', [SavesController, 'store'])
+    router.delete('sites/:site_id/saves/:save_id', [SavesController, 'destroy'])
+  })
+  .use(middleware.auth())
+
+// Add and delete visites
+router
+  .group(() => {
+    router.post('sites/:site_id/visites', [VisitesController, 'store'])
+    router.put('sites/:site_id/visites/:visite_id', [VisitesController, 'update'])
+    router.delete('sites/:site_id/visites/:visite_id', [VisitesController, 'destroy'])
+  })
+  .use(middleware.auth())
+
+// CRUD sites
+router
+  .group(() => {
+    router.get('', [SitesController, 'index'])
+    router.get(':book_id', [SitesController, 'show'])
+    router.post('', [SitesController, 'store']).use(middleware.auth())
+    router.put(':book_id', [SitesController, 'update']).use(middleware.auth())
+    router.delete(':book_id', [SitesController, 'destroy']).use(middleware.auth())
+  })
+  .prefix('sites')
