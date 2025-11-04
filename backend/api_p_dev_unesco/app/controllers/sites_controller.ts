4993fb46b6f86ec671f79e4addeebfa22128f814
mina-etml <pk60ftv@eduvaud.ch>
2025-11-04 10:33:35 +0100

    feat(site): creating site contoller and site query validator

--- backend/api_p_dev_unesco/app/controllers/sites_controller.ts
+++ backend/api_p_dev_unesco/app/controllers/sites_controller.ts
@@ -1,4 +1,46 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Site from '#models/site'
+import { getSitesQueryValidator } from '#validators/visite'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class SitesController {
+  async index({ response, request }: HttpContext) {
+    //  Retrieves the pagination parameters from the query
+    const {
+      page = 1,
+      limit = 10,
+      sort = 'name',
+      order = 'asc',
+      countryFK,
+      typeFK,
+      search,
+    } = await request.validateUsing(getSitesQueryValidator)
+
+    const query = Site.query().preload('country').preload('type')
+    if (countryFK) {
+      query.where('class_group_id', countryFK) // Filter by country ID
+    }
+
+    // Search
+    if (search) {
+      query.where((subQuery) => {
+        subQuery.whereILike('name', `%${search}%`) // Faire la même chose pour trouver le nom du pays .orWhereILike('country_fk', `%${search}%`)
+      })
+    }
+
+    // Sort site
+    query.orderBy(sort, order as 'asc' | 'desc')
+
+    const sites = await query.paginate(page, limit) // Results pagination
+    sites.baseUrl('/sites')
+
+    // Keep search parameters
+    sites.queryString({ page, limit, sort, order, countryFK, typeFK, search })
+
+    return response.ok(sites)
+  }
+
+  async show({ params, response }: HttpContext) {
+    const sites = await Site.findOrFail(params.id)
+    return response.ok(sites)
+  }
 }
